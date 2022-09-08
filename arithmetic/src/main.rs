5799ed82c14c780337fb92cf81960cd6fad8de9c
Trollkaaar <albin.woxnerud@gmail.com>
2022-09-08 23:03:27 +0200

    forgot about dif

--- arithmetic/src/main.rs
+++ arithmetic/src/main.rs
@@ -1,15 +1,20 @@
 fn main() {
     println!("Hello, world!");
-    let sum_of_ints = sum_func(10, 20);
+    let sum_of_ints = sum(10, 20);
     println!("10 + 20 = {sum_of_ints}");
-    let average_of_floats = average_func(12.5, 17.2); //good enough
+    let difference = difference(3, 5);
+    println!("Difference of 3 and 5 = {difference}");
+    let average_of_floats = average(12.5, 17.2); //good enough
     println!("Average of 12.5 and 17.2 = {average_of_floats}");
 }
 
-fn sum_func(a: i32, b: i32) -> i32 {
+fn sum(a: i32, b: i32) -> i32 {
     a + b
 }
 
-fn average_func(a: f32, b: f32) -> f32 {
+fn difference(a: i32, b: i32) -> i32 {
+    a - b
+}
+fn average(a: f32, b: f32) -> f32 {
     return (a + b) * 0.5;
 }
