addde62c11d6741b512ae4c47a50e334d7ba1bfc
TrollKaaar <albin.woxnerud@gmail.com>
2022-09-24 03:17:08 +0200

    attack maps almost done, i hate bishops and rooks and queens

--- test_chess_features/src/main.rs
+++ test_chess_features/src/main.rs
@@ -1,6 +1,21 @@
 fn main() {
-    let white_pawn = 1u64 << 8;
-    print_bitboard(mask_pawn_attack(Sides::WHITE, SquareLabels::A8 as u8));
+    let pawn_attack_table: [[u64; 64]; 2] = generate_pawn_attack_tables();
+    let knight_attack_table: [u64; 64] = generate_knight_attack_tables();
+    let king_attack_table: [u64; 64] = generate_king_attack_tables();
+    let bishop_attack_table: [u64; 64] = generate_bishop_attack_tables();
+    // let white_pawn = 1u64 << 8;
+
+    // for x in 0..2 {
+    //     for y in 0..64 {
+    //         print_bitboard(pawn_attack_table[x][y])
+    //     }
+    // }
+    // print_bitboard(mask_rook_attack(SquareLabels::D4 as usize));
+    for x in 0..64 {
+        print_bitboard(mask_rook_attack(x));
+        println!("{}", x);
+    }
+    // println!("{:?}", bishop_attack_table);
 
     // let mut bb = 0u64;
     // for x in 0..8 {
@@ -14,13 +29,12 @@ fn main() {
     // println!("{}", bb);
     // print_bitboard(bb)
 }
-const not_a: u64 = 18374403900871474942;
-const not_ab: u64 = 18229723555195321596;
-const not_h: u64 = 9187201950435737471;
-const not_gh: u64 = 4557430888798830399;
+const NOT_A: u64 = 18374403900871474942;
+const NOT_AB: u64 = 18229723555195321596;
+const NOT_H: u64 = 9187201950435737471;
+const NOT_GH: u64 = 4557430888798830399;
 
 fn print_bitboard(bb: u64) {
-    println!("works");
     for rank in 0..8 {
         print!("{} ", 8 - rank);
         for file in 0..8 {
@@ -38,19 +52,209 @@ fn print_bitboard(bb: u64) {
     }
     println!("  A B C D E F G H");
 }
-pub fn mask_pawn_attack(side: usize, square_index: u8) -> u64 {
+fn mask_pawn_attack(side: usize, square_index: usize) -> u64 {
     let mut pawn_attacks = 0u64;
     let mut bb = 0u64;
-    bb |= (1u64 << square_index);
-    print_bitboard(bb);
-    if (side == Sides::WHITE) {
-        // if()
+    bb |= 1u64 << square_index;
+    if side == Sides::WHITE {
+        if (bb >> 7) & NOT_A != 0 {
+            pawn_attacks |= bb >> 7
+        }
+        if (bb >> 9) & NOT_H != 0 {
+            pawn_attacks |= bb >> 9
+        }
     } else {
+        if (bb << 7) & NOT_H != 0 {
+            pawn_attacks |= bb << 7
+        }
+        if (bb << 9) & NOT_A != 0 {
+            pawn_attacks |= bb << 9
+        }
     }
 
     pawn_attacks
 }
-pub struct BitBoard(pub u64);
+fn mask_knight_attack(square_index: usize) -> u64 {
+    let mut pawn_attacks = 0u64;
+    let mut bb = 0u64;
+    bb |= 1u64 << square_index;
+
+    if (bb >> 17) & NOT_H != 0 {
+        pawn_attacks |= bb >> 17
+    }
+    if (bb >> 15) & NOT_A != 0 {
+        pawn_attacks |= bb >> 15
+    }
+    if (bb >> 10) & NOT_GH != 0 {
+        pawn_attacks |= bb >> 10
+    }
+    if (bb >> 6) & NOT_AB != 0 {
+        pawn_attacks |= bb >> 6
+    }
+
+    if (bb << 17) & NOT_A != 0 {
+        pawn_attacks |= bb << 17
+    }
+    if (bb << 15) & NOT_H != 0 {
+        pawn_attacks |= bb << 15
+    }
+    if (bb << 10) & NOT_AB != 0 {
+        pawn_attacks |= bb << 10
+    }
+    if (bb << 6) & NOT_GH != 0 {
+        pawn_attacks |= bb << 6
+    }
+
+    pawn_attacks
+}
+
+fn mask_king_attack(square_index: usize) -> u64 {
+    let mut king_attacks = 0u64;
+    let mut bb = 0u64;
+    bb |= 1u64 << square_index;
+
+    if (bb >> 9) & NOT_H != 0 {
+        king_attacks |= bb >> 9
+    }
+    if (bb >> 8) != 0 {
+        king_attacks |= bb >> 8
+    }
+    if (bb >> 7) & NOT_A != 0 {
+        king_attacks |= bb >> 7
+    }
+    if (bb >> 1) & NOT_H != 0 {
+        king_attacks |= bb >> 1
+    }
+
+    if (bb << 9) & NOT_A != 0 {
+        king_attacks |= bb << 9
+    }
+    if (bb << 8) != 0 {
+        king_attacks |= bb << 8
+    }
+    if (bb << 7) & NOT_H != 0 {
+        king_attacks |= bb << 7
+    }
+    if (bb << 1) & NOT_A != 0 {
+        king_attacks |= bb << 1
+    }
+
+    king_attacks
+}
+
+///Magic, https://www.chessprogramming.org/Magic_Bitboards
+fn mask_bishop_attack(square_index: usize) -> u64 {
+    let mut bishop_attacks = 0u64;
+    let tmp: i8 = square_index.try_into().unwrap();
+    let target_rank: i8 = tmp / 8;
+    let target_file: i8 = tmp % 8;
+
+    let mut r: i8;
+    let mut f: i8;
+
+    r = target_rank + 1;
+    f = target_file + 1;
+    while r <= 6 && f <= 6 {
+        bishop_attacks |= 1u64 << (r * 8 + f);
+        r += 1;
+        f += 1;
+    }
+
+    r = target_rank - 1;
+    f = target_file + 1;
+    while r >= 1 && f <= 6 {
+        bishop_attacks |= 1u64 << (r * 8 + f);
+        r -= 1;
+        f += 1;
+    }
+
+    r = target_rank + 1;
+    f = target_file - 1;
+    while r <= 6 && f >= 1 {
+        bishop_attacks |= 1u64 << (r * 8 + f);
+        r += 1;
+        f -= 1;
+    }
+
+    r = target_rank - 1;
+    f = target_file - 1;
+    while r >= 1 && f >= 1 {
+        bishop_attacks |= 1u64 << (r * 8 + f);
+        r -= 1;
+        f -= 1;
+    }
+
+    bishop_attacks
+}
+
+///Magic, https://www.chessprogramming.org/Magic_Bitboards
+fn mask_rook_attack(square_index: usize) -> u64 {
+    let mut rook_attacks = 0u64;
+    let tmp: i8 = square_index.try_into().unwrap();
+    let target_rank: i8 = tmp / 8;
+    let target_file: i8 = tmp % 8;
+
+    let mut r: i8;
+    let mut f: i8;
+
+    r = target_rank + 1;
+    while r <= 6 {
+        rook_attacks |= 1u64 << (r * 8 + target_file);
+        r += 1;
+    }
+
+    r = target_rank - 1;
+    while r >= 1 {
+        rook_attacks |= 1u64 << (r * 8 + target_file);
+        r -= 1;
+    }
+
+    f = target_file + 1;
+    while f <= 6 {
+        rook_attacks |= 1u64 << (target_rank * 8 + f);
+        f += 1;
+    }
+
+    f = target_file - 1;
+    while f >= 1 {
+        rook_attacks |= 1u64 << (target_rank * 8 + f);
+        f -= 1;
+    }
+
+    rook_attacks
+}
+
+fn generate_pawn_attack_tables() -> [[u64; 64]; 2] {
+    let mut pawn_attack_table_white: [u64; 64] = [0; 64];
+    let mut pawn_attack_table_black: [u64; 64] = [0; 64];
+    for x in 0..64 {
+        pawn_attack_table_white[x] = mask_pawn_attack(Sides::WHITE, x);
+        pawn_attack_table_black[x] = mask_pawn_attack(Sides::BLACK, x);
+    }
+    [pawn_attack_table_white, pawn_attack_table_black]
+}
+
+fn generate_knight_attack_tables() -> [u64; 64] {
+    let mut knight_attack_table: [u64; 64] = [0; 64];
+    for x in 0..64 {
+        knight_attack_table[x] = mask_knight_attack(x);
+    }
+    knight_attack_table
+}
+fn generate_king_attack_tables() -> [u64; 64] {
+    let mut king_attack_table: [u64; 64] = [0; 64];
+    for x in 0..64 {
+        king_attack_table[x] = mask_king_attack(x);
+    }
+    king_attack_table
+}
+fn generate_bishop_attack_tables() -> [u64; 64] {
+    let mut bishop_attack_table: [u64; 64] = [0; 64];
+    for x in 0..64 {
+        bishop_attack_table[x] = mask_bishop_attack(x);
+    }
+    bishop_attack_table
+}
 
 pub struct Sides;
 impl Sides {
@@ -68,8 +272,12 @@ impl Pieces {
     pub const KING: usize = 5;
 }
 pub struct Position {
-    bb_sides: [BitBoard; 2],
-    bb_pieces: [[BitBoard; 6]; 2],
+    bb_sides: [u64; 2],
+    bb_pieces: [[u64; 6]; 2],
+}
+
+pub struct AttackTables {
+    attack_table: [[u64; 64]; 2],
 }
 
 pub enum SquareLabels {
